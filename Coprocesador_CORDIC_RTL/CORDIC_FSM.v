094ecbed35d238dfa9188648e0668c804cf992ef
JeffryQF <jeffryq.9@gmail.com>
2016-03-01 08:44:29 -0600

    Cambios
    Se elimino la señal de enable del modulo de LUT, ya que ya no existe
    como tal, ademas de que se comento parte del codigo, en especial las
    señales del modulo

--- Coprocesador_CORDIC_RTL/CORDIC_FSM.v
+++ Coprocesador_CORDIC_RTL/CORDIC_FSM.v
@@ -23,31 +23,31 @@
 module CORDIC_FSM
 (
 //Input Signals 
-input wire clk,
-input wire reset,
-input wire beg_FSM_CORDIC,
-input wire ACK_FSM_CORDIC,
-input wire operation,
-input wire [1:0] shift_region_flag,
-input wire [1:0] cont_var,
-input wire ready_add_subt,
-input wire max_tick_iter, min_tick_iter,
-input wire max_tick_var, min_tick_var,
+input wire clk,											//	Reloj del sitema.
+input wire reset,										//	Reset del sitema.
+input wire beg_FSM_CORDIC,								//	Señal de inicio de la maquina de estados.
+input wire ACK_FSM_CORDIC,								//	Señal proveniente del modulo que recibe el resultado, indicado que el dato ha sido recibido.
+input wire operation,									//	Señal que determina si lo que se requiere es realizar un coseno(1´b0) o seno (1'b1).
+input wire [1:0] shift_region_flag,						//	Señal que indica si el angulo a calcular se encuentra fuera del rango de calculo del algoritmo CORDIC.
+input wire [1:0] cont_var,								//	Señal que indica cual varible se va a calcular. Proveniente del contador de variables.
+input wire ready_add_subt,								//	Señal proveniente del módulo de suma/resta, indica que se ha terminado la operacion y que se puede disponer del resultado de dicho modulo.
+input wire max_tick_iter, min_tick_iter,				//	Señales que indican la maxima y minima cuenta, respectivamente, en el contador de iteraciones.
+input wire max_tick_var, min_tick_var,					//	Señales que indican la maxima y minima cuenta, respectivamente, en el contador de variables.
 
 //Output Signals
-output reg ready_CORDIC,
-output reg beg_add_subt,
-output reg ack_add_subt,
-output reg sel_mux_1, sel_mux_3,
-output reg [1:0] sel_mux_2,
-output reg mode,
-output reg enab_cont_iter, load_cont_iter,
-output reg enab_cont_var,  load_cont_var,
-output reg enab_RB1, enab_RB2, enab_LUT,
-output reg enab_d_ff_Xn, enab_d_ff_Yn, enab_d_ff_Zn,
-output reg enab_d_ff_out, enab_dff5,
-output reg enab_dff_shifted_x, enab_dff_shifted_y,
-output reg enab_dff_LUT, enab_dff_sign
+output reg ready_CORDIC,								//	Señal que indica que el calculo CORDIC se ha terminado.
+output reg beg_add_subt,								//	Señal que indica al modulo de suma/resta que inicie su operacion.
+output reg ack_add_subt,								//	Señal que le indica al modulo de suma/resta que se ha recibido exitosamente el resultado que este entrega.
+output reg sel_mux_1, sel_mux_3,						//	Señales de seleccion de mux, la primera escoge el canal 0 si es la primera iteracion, en otro caso escoge el canal 1, y la segunda escoge cual variable (X o Y) debe aparecer a la salida.
+output reg [1:0] sel_mux_2,								//	Señal de seleccion de mux, que escoge entre X, Y o Z dependiendo de cual variable se deba calcular en ese momento.
+output reg mode,										//	1'b0 si el modo es rotacion(signo de Y), 1'b1 si el modo es vectorizacion(signo de Z).
+output reg enab_cont_iter, load_cont_iter,				//	Señales de habilitacion y carga, respectivamente, en el contador de iteraciones.
+output reg enab_cont_var,  load_cont_var,				//	Señales de habilitacion y carga, respectivamente, en el contador de variables.
+output reg enab_RB1, enab_RB2,							//	Señales de habilitacion para los registros de variables de entrada y para los valores de las variables despues de los primeros mux, respectivamente.
+output reg enab_d_ff_Xn, enab_d_ff_Yn, enab_d_ff_Zn,	//	Señales de habilitacion para los registros que guardan los resultados de cada variable en cada iteracion provenientes del modulo de suma/resta.
+output reg enab_dff5, enab_d_ff_out,					//	Señales de habilitacion para los registros en la salida, el primero antes del cambio de signo y el segundo es el que se encuentra en la salida.
+output reg enab_dff_shifted_x, enab_dff_shifted_y,		//	Señales de habilitacion para los registros que guardan el valor de las variables X y Y luego de realizarles los desplazamientos.
+output reg enab_dff_LUT, enab_dff_sign					//	Señales de habilitacion para los registros	que guardan los valores provenientes de la look-up table y del signo, respectivamente.
 );
 
 //symbolic state declaration
@@ -65,15 +65,15 @@ localparam [3:0]    est0 = 4'b0000,
                     est11 = 4'b1011;
 
 //signal declaration
-reg state_reg, state_next;
+reg state_reg, state_next;	//	Guardan el estado actual y el estado futuro, respectivamente.
 
 //state register
 
 always @(clk, reset)
     begin
-        if(reset)
+        if(reset)	// Si hay reset, el estado actual es el estado inicial.
             state_reg <= est0;
-        else
+        else		//Si no hay reset el estado actual es igual al estado siguiente.
             state_reg <= state_next;
     end
 
@@ -82,6 +82,7 @@ always @(clk, reset)
 always@*
     begin
     state_next = state_reg; // default state : the same
+	
     //declaration of default outputs. 
     ready_CORDIC = 1'b0;
     beg_add_subt = 1'b0;
@@ -96,7 +97,6 @@ always@*
     load_cont_var = 1'b0;
     enab_RB1 = 1'b0;
     enab_RB2 = 1'b0;
-    enab_LUT = 1'b0;
     enab_d_ff_Xn = 1'b0;
     enab_d_ff_Yn = 1'b0;
     enab_d_ff_Zn = 1'b0;
