07ab6f3dc56f7e9d0dc6b0228c5683d8145896f8
JeffryQF <jeffryq.9@gmail.com>
2016-03-01 09:09:30 -0600

    Nuevo instanciacion
    Modulo que unira la arquitectura cordic con el modulo de suma_resta

---
+++ Coprocesador_CORDIC_RTL/Sine_Cosine_CORDIC.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.03.2016 09:07:25
+// Design Name: 
+// Module Name: Sine_Cosine_CORDIC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Sine_Cosine_CORDIC(
+
+    );
+endmodule
