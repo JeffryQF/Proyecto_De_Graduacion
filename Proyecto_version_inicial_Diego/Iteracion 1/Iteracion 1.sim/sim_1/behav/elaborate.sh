c9a13e6832804d488259cba2762850fa8a7ec485
flopezmontero <falm81427@gmail.com>
2016-03-08 00:15:36 -0600

    FPU_FLM Start_op_in

--- Proyecto_version_inicial_Diego/Iteracion 1/Iteracion 1.sim/sim_1/behav/elaborate.sh
+++ Proyecto_version_inicial_Diego/Iteracion 1/Iteracion 1.sim/sim_1/behav/elaborate.sh
@@ -9,4 +9,4 @@ then
 exit $RETVAL
 fi
 }
-ExecStep $xv_path/bin/xelab -wto 36f1cfea73054869b536686a8cc3f809 -m64 --debug typical --relax --mt 8 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot TB32_ZDetector_behav xil_defaultlib.TB32_ZDetector xil_defaultlib.glbl -log elaborate.log
+ExecStep $xv_path/bin/xelab -wto 36f1cfea73054869b536686a8cc3f809 -m64 --debug typical --relax --mt 8 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot Temporal_op_result_sign_behav xil_defaultlib.Temporal_op_result_sign xil_defaultlib.glbl -log elaborate.log
--- Proyecto_version_inicial_Diego/Iteracion 1/Iteracion 1.sim/sim_1/behav/simulate.sh
+++ Proyecto_version_inicial_Diego/Iteracion 1/Iteracion 1.sim/sim_1/behav/simulate.sh
@@ -9,4 +9,4 @@ then
 exit $RETVAL
 fi
 }
-ExecStep $xv_path/bin/xsim TB32_ZDetector_behav -key {Behavioral:sim_1:Functional:TB32_ZDetector} -tclbatch TB32_ZDetector.tcl -log simulate.log
+ExecStep $xv_path/bin/xsim Temporal_op_result_sign_behav -key {Behavioral:sim_1:Functional:Temporal_op_result_sign} -tclbatch Temporal_op_result_sign.tcl -view /media/francis/Acer/Users/Francis/Documents/GitHub/Proyecto_De_Graduacion/Proyecto_version_inicial_Diego/Iteracion 1/TB32_ZDetector_behav.wcfg -log simulate.log
