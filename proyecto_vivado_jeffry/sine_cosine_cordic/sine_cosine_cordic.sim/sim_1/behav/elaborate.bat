fe224af10d04e987f47ce1cb91363ed652eb981d
JeffryQF <jeffryq.9@gmail.com>
2016-03-18 19:51:48 -0600

    simulación
    simulación

--- proyecto_vivado_jeffry/sine_cosine_cordic/sine_cosine_cordic.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.4\\bin
-call %xv_path%/xelab  -wto 0b5b83e26f46491bbf4f514cbd60d3f5 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot CORDIC_FSM_v2_tb_behav xil_defaultlib.CORDIC_FSM_v2_tb xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- proyecto_vivado_jeffry/sine_cosine_cordic/sine_cosine_cordic.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.4\\bin
-call %xv_path%/xsim CORDIC_FSM_v2_tb_behav -key {Behavioral:sim_1:Functional:CORDIC_FSM_v2_tb} -tclbatch CORDIC_FSM_v2_tb.tcl -view C:/Users/Jeffry/Dropbox/Proyecto_De_Graduacion/Proyecto_De_Graduacion_2/Proyecto_De_Graduacion/proyecto_vivado_jeffry/sine_cosine_cordic/CORDIC_FSM_v2_tb_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
